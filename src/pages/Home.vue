6af7923bc7b6d4b5989e905ef9bc2125cd064b36
Stefan Yilmaz <sy@suleymanyilmaz.org>
2021-08-09 19:46:06 +0300

    Update home to list pokemon.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,32 @@
 <template>
-  Home Page
+  Pokemons:
+  <ul>
+    <li v-for="pokemon in pokemons">
+      <router-link
+          :to="{ name: 'pokemonDetail', params: { id: pokemon.id }}"
+      >
+        {{ pokemon.name }}
+      </router-link>
+    </li>
+  </ul>
 </template>
 
 <script>
-import {defineComponent} from 'vue';
+import {computed, defineComponent, onBeforeMount} from 'vue';
 import {useHead} from '@vueuse/head';
+import {useStore} from 'vuex'
 
 export default defineComponent({
+  serverPrefetch() {
+    return this.$store.dispatch('pokemon/FETCH_POKEMONS');
+  },
   setup() {
+    const store = useStore()
+    const pokemons = computed(() => store.getters['pokemon/pokemons'])
+
+    onBeforeMount(() => {
+      store.dispatch('pokemon/FETCH_POKEMONS')
+    });
     useHead({
       title: 'Home Page',
       meta: [
@@ -17,10 +36,10 @@ export default defineComponent({
         },
       ],
     });
+
+    return {
+      pokemons,
+    }
   },
 });
 </script>
-
-<style scoped>
-
-</style>
