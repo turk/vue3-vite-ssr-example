12e54c817b3a6800ceab1ede5a368d10f11be4b6
Stefan Yilmaz <sy@suleymanyilmaz.org>
2021-08-09 19:44:46 +0300

    Update vite.config.js to use .env

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
+require('dotenv').config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+
     resolve: {
         alias: {
             '@': path.resolve(__dirname, '/src'),
         },
     },
-    define: {'process.env': {}},
+    define: {
+        'process.env': process.env,
+    },
     /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
     resolve: {
       extensions: [
